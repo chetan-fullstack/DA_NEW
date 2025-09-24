221858f87a85a472bd9bf748026a5983916e2c44
Chetan-Troopy <chetan.fullstack@gmail.com>
2025-09-24 23:40:33 +0530

    Lecture Data

---
+++ Data_Analyst/Module_Python/Matplotlib/01Lecture/index.py
@@ -0,0 +1,32 @@
+import matplotlib.pyplot as plt
+
+
+
+x = [1, 2, 3, 4,5]
+y = [10, 20, 30, 20, 10]
+
+# plt.plot(x, y)
+# plt.show()
+
+
+
+# plt.plot(x, y, color='red', linestyle='--', marker='s')
+# plt.show()
+
+
+# data = [1,2,2,3,3,3,4,4,4,4,5,5,6,7,8,8,9]
+# plt.hist(data, bins=5, color="orange", edgecolor="blue")
+# plt.show()
+
+
+
+# plt.plot(x, y)
+# plt.title("Growth of Sales")
+# plt.xlabel("Years")
+# plt.ylabel("Sales")
+# plt.show()
+
+
+# plt.plot(x, y, label="Sales")
+# plt.legend()
+# plt.show()
